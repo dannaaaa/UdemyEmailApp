8d267ed07985718aa2b66ee146faea2b6eee36d9
Dana <danaawua@hotmail.co.uk>
2019-11-28 16:17:25 +0000

    Added mwthod that generates random password

--- src/main/java/com/spartaglobal/UdemyEmailApp/Email.java
+++ src/main/java/com/spartaglobal/UdemyEmailApp/Email.java
@@ -9,6 +9,7 @@ public class Email {
     private String lastName;
     private String department;
     private String password;
+    private int defaultPassword = 9;
     private int mailboxCapacity;
 
 
@@ -20,7 +21,13 @@ public class Email {
 
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        this.password = randomPassword(defaultPassword);
+        System.out.println("Your password is: " + this.password);
     }
+
+    //call a method that return a random password
+
 
     private String setDepartment() {
         System.out.println("Enter department\n1 for Sales\n3 for Development\n3 for Accounting\n0 for none" );
@@ -36,8 +43,14 @@ public class Email {
             return "Please select a department";
     }
 
-    public String getPassword() {
-        return password;
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@$#%";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String (password);
     }
 
     public void setAlternateEmail(String alternateEmail) {
